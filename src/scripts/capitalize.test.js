9cf68c866bf65dc414b70a1cdb881e68575fc2da
Jar'Zeno <jarzenojarrett@gmail.com>
2022-12-16 10:58:37 -0800

    Add new test for capitalize

--- src/scripts/capitalize.test.js
+++ src/scripts/capitalize.test.js
@@ -3,3 +3,7 @@ const capitalize = require('./capitalize');
 test('capitalize h in hello to Hello', () => {
     expect(capitalize('hello')).toBe('Hello');
 });
+
+test('capitalizee e in essAy to EssAy', () => {
+    expect(capitalize('essAy')).toBe('EssAy');
+})
